da967b2273df7592544140c20fe9ee79c56d6043
Edgar Maniago <edgar.maniago@gmail.com>
2018-02-08 12:16:18 -0500

    2018-02-08 basic bot
    Added basic functionality

--- saptestbot.py
+++ saptestbot.py
@@ -16,3 +16,28 @@ COMMANDS = {
 def handle_command(cmd, channel):
     cmd = cmd.split()
     cmd, args = cmd[0], cmd[1:]
+
+    if cmd in COMMANDS:
+        if args:
+            response = COMMANDS[cmd](*args)
+        else:
+            response = COMMANDS[cmd]()
+    else:
+        response = ('Not sure what you mean'
+                    'I can help you with these commands:\n'
+                    '{}'.format('\n'.join(cmd_names)))
+
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+if __name__ == '__main__':
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("saptestbot connected and running")
+        while True:
+            command,channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
