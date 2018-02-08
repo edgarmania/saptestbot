7a409f777e19f3652c546ebbd7da3e652133cb16
Edgar Maniago <edgar.maniago@gmail.com>
2018-02-08 13:29:14 -0500

    2018-02-08 initial drop
    added get_botid.py

---
+++ get_botid.py
@@ -0,0 +1,29 @@
+import os
+from pprint import pprint as pp
+import sys
+
+from slackclient import SlackClient
+
+
+BOT_NAME = 'saptestbot'
+
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+
+if __name__ == "__main__":
+    '''
+    api_call = slack_client.api_call("channels.list")
+    for i in api_call.get('channels'):
+        print(i['id'], i['name'])
+    sys.exit()
+    '''
+
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
